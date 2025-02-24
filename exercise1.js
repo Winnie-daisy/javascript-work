161ff83202d461c53d24fd93cd4edd2110dece34
winnie daisy <winniedaisyaber@gmail.com>
2025-02-24 10:47:55 +0300

    Initial commit

---
+++ exercise1.js
@@ -0,0 +1,32 @@
+let a = 4;
+let b = 3;
+console.log(b>a);
+console.log(b>=a);
+console.log(b<a);
+console.log(a==a);
+console.log(a===a);
+console.log(a!=a);
+console.log(a!==a);
+console.log(a!='a');
+console.log(a=='a');
+console.log(a==='a');
+
+let firstName = "dragon";
+let SecondName = "javascript";
+let fullSentence = "there is no on in both"+`${firstName}+"and" + ${SecondName}`;
+console.log(fullSentence);
+
+
+let myAge = 250;
+let yourAge = 25;
+let ageDiff = myAge - yourAge;
+let sentence = "I am "+ageDiff+" older than you ";
+console.log(sentence);
+
+let text = "30 days of javascript";
+console.log(text);
+let upperText = text.toUpperCase();
+console.log(upperText);
+let lowerText = text.toLowerCase();
+console.log(lowerText);
+
---
+++ exercise2.js
@@ -0,0 +1,5 @@
+const currentMonth = new Date().getMonth() + 1;
+console.log(currentMonth);
+console.log(now.getDate());
+console.log(now.getDay());
+console.log(now.getHours());
